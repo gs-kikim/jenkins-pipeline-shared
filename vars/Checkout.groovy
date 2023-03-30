f671f42bd13f5a233a63c785d47a8c69b8626518
kikim <85283385+gs-kikim@users.noreply.github.com>
2023-03-30 16:53:02 +0900

    Update Checkout.groovy

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -5,18 +5,9 @@ import hudson.scm.SubversionSCM
 import hudson.scm.SubversionSCM.ModuleLocation
 
 
-class Checkout implements Serializable {
-
-    def script
-
-    Checkout() {
-
-    }
-
-    def call(String scmUrl, String branch, String checkoutDir) {
-        checkout(new SubversionSCM([
-                new ModuleLocation(scmUrl, branch, null, null, true)
-        ]), checkoutDir)
-    }
-
+def call(String scmUrl, String branch, String checkoutDir) {
+    checkout(new SubversionSCM([
+            new ModuleLocation(scmUrl, branch, null, null, true)
+    ]), checkoutDir)
 }
+
